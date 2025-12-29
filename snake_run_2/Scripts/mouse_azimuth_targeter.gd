e16ea87f0649e953379b2253e1255fdd4004a9c9
Mathew Hawley <mathewjonhawley@gmail.com>
2025-12-29 13:19:25 -0500

    added so the mouse turns his torsoe toward the enemy , next is animation

--- snake_run_2/Scripts/mouse_azimuth_targeter.gd
+++ snake_run_2/Scripts/mouse_azimuth_targeter.gd
@@ -2,11 +2,65 @@
 
 extends Node3D
 
+var angle :float = 0 
+
+@onready var snake_target :Marker3D = get_node("../../snake_head")
+
+@onready var skeleton_for_measure :Skeleton3D = get_node("../mouse_soldier2/Armature (Mecha g)_002/Skeleton3D")
 
 
-@onready var snake_target :Marker3D = get_node("../snake_head")
+var hips_integer :int = 0 
+var spine_integer :int = 0 
+
+@export var transform_1 :Transform3D 
+@export var transform_2 :Transform3D 
+
+@onready var orientation :Node3D = get_node("../mouse_soldier2")
+
+@onready var text_1 :Label3D = get_node("../../Label3D")
+
+@onready var parent :Node3D = get_node("../mouse_soldier2")
+
+
+	
+func _ready() -> void:
+	
+
+	
+	for i in skeleton_for_measure.get_bone_count():
+		var immediate_name :String = skeleton_for_measure.get_bone_name(i)
+		if immediate_name == "Spine":
+			hips_integer = i
+		if immediate_name == "Chest":
+			spine_integer = i
+	# 
+	
+	
+
+
 
 func _process(delta: float) -> void:
 	
-	
+	transform_1 = skeleton_for_measure.get_bone_global_pose_no_override(hips_integer)
+	#transform_2 = skeleton_for_measure.get_bone_global_pose_no_override(spine_integer)
+	transform_2 = self.global_transform
 	self.look_at(snake_target.global_position, Vector3(0, 1, 0))
+	
+	var current_angle :float = self.rotation_degrees.y + 180
+	
+	text_1.text = str(fmod(orientation.rotation_degrees.y + 360,  360)) + "\n" + str(self.rotation_degrees.y + 180)  
+
+	if current_angle > 45 and current_angle <= 135 :
+		parent.set_rotation_degrees(Vector3(0,90,0)) 
+	elif current_angle > 135 and current_angle <= 225 :
+		
+		parent.set_rotation_degrees(Vector3(0,180,0)) 
+		
+	elif current_angle > 225 and current_angle <= 315 :
+		parent.set_rotation_degrees(Vector3(0,270,0)) 
+		
+	elif current_angle > 315 or current_angle <= 45 :
+		parent.set_rotation_degrees(Vector3(0,0,0)) 
+	else:
+		pass
+		
